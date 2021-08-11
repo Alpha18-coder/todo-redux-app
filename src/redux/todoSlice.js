ee3f8e8532033e11a963a2144b3d7e7bc62ec31b
AsherCode <alonsosandovalch@gmail.com>
2021-08-11 11:39:27 -0400

    first commit

---
+++ src/redux/todoSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { nanoid } from 'nanoid';
+
+export const todoSlice = createSlice({
+    name: 'todos',
+    initialState: [
+        {id: 1, title: 'todo', completed: false},
+        {id: 2, title: 'todo2', completed: true},
+    ],
+    reducers: {
+        addTodo: (state, action) => {
+			const todo = {
+				id: nanoid(),
+				title: action.payload.title,
+				completed: false,
+			};
+			state.push(todo);
+		},
+		toggleComplete: (state, action) => {
+			const index = state.findIndex((todo) => todo.id === action.payload.id);
+			state[index].completed = action.payload.completed;
+		},
+
+        deleteTodo: (state, action) => {
+            return state.filter((todo) => todo.id !== action.payload.id);
+        }
+    },
+});
+
+
+export const { addTodo, toggleComplete, deleteTodo } = todoSlice.actions;
+export default todoSlice.reducer;
